e9751c071d1d63396957ea3fd5800890239b9019
Florin Mihalache <florinrm@users.noreply.github.com>
2021-10-28 21:02:09 +0300

    Added notify

--- src/com/luxoft/bankapp/service/EmailService.java
+++ src/com/luxoft/bankapp/service/EmailService.java
@@ -50,6 +50,9 @@ public class EmailService implements Runnable {
 
     public void close() {
         isServiceClosed = true;
+        synchronized (emailStorage) {
+            emailStorage.notify();
+        }
         try {
             currentThread.join();
         } catch (InterruptedException e) {
